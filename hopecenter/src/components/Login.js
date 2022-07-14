07a24ae0f6b30318effb59bd00cf1e2aefbaf7ce
AzyaBurgess <Azya.Burgess@cdpipelinedevshop.com>
2022-07-14 17:01:15 +0000

    first commit

---
+++ hopecenter/src/components/Login.js
@@ -0,0 +1,63 @@
+//Login.js
+import React from "react";
+import Grid from "@mui/material/Grid";
+import { Paper, Avatar, TextField, Checkbox, Button, Typography, Link } from "@mui/material";
+import LockIcon from "@mui/icons-material/Lock";
+import FormControlLabel from "@mui/material/FormControlLabel";
+const Login=()=> {
+    const paperStyle={
+        padding: "20px",
+        height: "70vh",
+        width: 280,
+        margin: "20px auto"
+    }
+    const avatarStyle={
+        backgroundColor:"green"
+    }
+    const btnstyle={
+        margin:"8px 0"
+    }
+    const textfield={
+        margin:"10px"
+    }
+    return(
+         <Grid>
+            <Paper elevation={10} style={paperStyle}>
+                <Grid align="center">
+                
+                <h2>The Hope Center</h2>
+                </Grid>
+        <TextField label="Username" placeholder="Enter Username" fullWidth required></TextField>
+        <FormControlLabel
+            control={
+                <Checkbox
+                    name="Checked"
+                    color="primary"
+            />}
+            label="Remember me"
+            />
+            <Button
+                type="Submit"
+                color="primary"
+                variant="contained"
+                style={btnstyle}
+                fullWidth
+                >
+                    Reset Password
+            </Button>
+            <Typography style={textfield}>
+                    <Link href="#">
+                        
+                    </Link>
+            </Typography>
+            <Typography style={textfield}>
+                    <Link href="#">
+                       
+                    </Link>
+            </Typography>
+        </Paper>
+        </Grid>
+    )
+}
+export default Login;
+
